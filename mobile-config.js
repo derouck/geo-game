8698878c0d1468405cc38df186befb043af2a001
Alexis d'Oultremont <alexis@qustomer.com>
2016-02-26 19:12:37 +0000

    gamesList + collections

--- mobile-config.js
+++ mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
 	id: 'com.beaconhunt.be',
-	name: 'Beacon Hunt',
+	name: 'GeoGame Hunt',
 	author: 'alexis,chris,jesper',
 	description: 'Beacon hunt for the 2nd project of the Meteor in flip flops bootcamp',
 	version: '0.0.1'
