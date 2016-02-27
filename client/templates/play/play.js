57e9491e9d7e3b84cfedad002b8c276d49065363
Alexis d'Oultremont <alexis@qustomer.com>
2016-02-27 12:56:21 +0000

    gameplay work

--- client/templates/play/play.js
+++ client/templates/play/play.js
@@ -1,16 +1,39 @@
+const getCurrentResult = () => {
+
+  return Result.find({userId:Meteor.userId()});
+}
+
+const getCurrentGame = () => {
+
+  return Games.find({_id:getCurrentResult().fetch().id});
+}
+
+const getCurrentGameWaypoints = () => {
+
+  //let gameId = getCurrentGame().fetch().id;
+  let gameId = "3aP2G43Dfge9E7Wib";
+  return Waypoints.find({gameId:gameId}).fetch();
+}
+
 Template.map.onCreated(function() {
 
-  this.subscribe('waypoints');
+  const self = this;
+  const handle  = this.subscribe('waypoints');
+  self.autorun(() => {
+    const isReady = handle.ready();
+    console.log(`Handle is ${isReady ? 'ready' : 'not ready'}`);
+  });
 
-  if(this.subscriptionsReady()){
-    console.log("Subs ready");
-  }
-  else {
-    console.log("Subs not ready");
-  }
 });
 
 
-Template.map.helpers({
-
+Template.play.helpers({
+  waypoints(){
+    let wps = getCurrentGameWaypoints();
+    return wps;
+  },
+  resultId(){
+    let wps = getCurrentResult().fetch().id;
+    return wps;
+  }
 });
